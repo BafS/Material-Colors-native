fcfb1888f4122332abf6f2cedff27a2dc093f91d
BafS <fabacrans@hotmail.com>
2016-03-28 15:06:16 +0200

    Better ColorCircle selector

--- index.osx.js
+++ index.osx.js
@@ -7,19 +7,28 @@ import React, {
 } from 'react-native-desktop';
 
 import ColorsPanel from './src/ColorsPanel';
-
 import { colors } from './src/colors';
 
-const ColorCircle = ({ name, color, selected }) => {
+const ColorCircle = ({ color, selected }) => {
   return (
     <View style={{
         width: 15,
         height: 15,
         margin: 5,
-        opacity: 1 - selected / 1.2,
         borderRadius: 20,
-        backgroundColor: color
-      }} />
+        backgroundColor: color,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }} >
+      {selected ?
+      <View style={{
+          width: 23,
+          height: 23,
+          borderWidth: 2,
+          borderColor: color,
+          borderRadius: 25,
+        }} /> : null}
+    </View>
   );
 };
 
@@ -27,11 +36,13 @@ const ColorsListPanel = ({ colors, style, onClick, selected }) => {
   return (
     <View style={style}>
     {Object.keys(colors).map((value, index) => {
-      let isSelected = value === selected;
+      const isSelected = value === selected;
       return (
         <TouchableOpacity
-          onPress={() => onClick(value)} key={index}>
-          <ColorCircle name={value} color={colors[value]['500'].color} selected={isSelected} />
+          key={index}
+          activeOpacity={0.6}
+          onPress={() => onClick(value)} >
+          <ColorCircle {...colors[value]['500']} selected={isSelected} />
         </TouchableOpacity>
       );
     })}
@@ -60,8 +71,15 @@ class MaterialColors extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ColorsListPanel style={styles.leftPane} colors={colors} selected={this.state.colorName} onClick={this.changeColor} />
-        <ColorsPanel style={styles.rightPane} name={this.state.colorName} palette={colors[this.state.colorName]} />
+        <ColorsListPanel
+          style={styles.leftPane}
+          colors={colors}
+          selected={this.state.colorName}
+          onClick={this.changeColor} />
+        <ColorsPanel
+          style={styles.rightPane}
+          name={this.state.colorName}
+          palette={colors[this.state.colorName]} />
       </View>
     );
   }
@@ -73,7 +91,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    backgroundColor: '#ffffff',
+    backgroundColor: '#fff',
   },
 
   leftPane: {
