5c3f4fbbf18fbf9d6944f3be389e60554272a03f
BafS <fabacrans@hotmail.com>
2016-03-29 19:05:01 +0200

    Clean the code

--- index.osx.js
+++ index.osx.js
@@ -3,52 +3,11 @@ import React, {
   StyleSheet,
   View,
   Clipboard,
-  TouchableOpacity
 } from 'react-native-desktop';
 
 import ColorsPanel from './src/ColorsPanel';
+import ColorsListPanel from './src/ColorsListPanel';
 import { colors } from './src/colors';
-
-const ColorCircle = ({ color, selected }) => {
-  return (
-    <View style={{
-        width: 15,
-        height: 15,
-        margin: 5,
-        borderRadius: 20,
-        backgroundColor: color,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }} >
-      {selected ?
-      <View style={{
-          width: 23,
-          height: 23,
-          borderWidth: 2,
-          borderColor: color,
-          borderRadius: 25,
-        }} /> : null}
-    </View>
-  );
-};
-
-const ColorsListPanel = ({ colors, style, onClick, selected }) => {
-  return (
-    <View style={style}>
-    {Object.keys(colors).map((value, index) => {
-      const isSelected = value === selected;
-      return (
-        <TouchableOpacity
-          key={index}
-          activeOpacity={0.6}
-          onPress={() => onClick(value)} >
-          <ColorCircle {...colors[value]['500']} selected={isSelected} />
-        </TouchableOpacity>
-      );
-    })}
-    </View>
-  );
-};
 
 class MaterialColors extends React.Component {
 
